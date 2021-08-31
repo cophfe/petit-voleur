443d9be2393ea67f0b13281c0fa0673e8a8f1ebc
Ryan <ryantjones2002@gmail.com>
2021-08-31 16:50:02 +1000

    Size Up Power up Script added

---
+++ Petit Voleur/Assets/Scripts/SizeUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeUp : MonoBehaviour
+{
+    public float multiplier = 1.5f;
+
+    //for if we want particles 
+    //public GameObject pickupEffect;
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider player)
+    {
+        //for if we want particles 
+        //Instantiate(pickupEffect, transform.position, transform.rotation);
+        
+        Debug.Log("ur mom");
+
+        player.transform.localScale *= multiplier;
+
+        Destroy(gameObject);
+    }
+}
