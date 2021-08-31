befcf8d3756e1ef2419aa72d2949de83a32abe06
cheese <rahul.jhugroo99@gmail.com>
2021-08-31 14:06:20 +1000

    added the ability to pick stuff up

--- Petit Voleur/Assets/Scripts/Item.cs
+++ Petit Voleur/Assets/Scripts/Item.cs
@@ -4,5 +4,40 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-	
+	public Rigidbody rbody;
+	private Collider col;
+	private Transform originalParent;
+	private RigidbodyInterpolation interpolateMode = RigidbodyInterpolation.None;
+
+	void Awake()
+	{
+		rbody = GetComponent<Rigidbody>();
+		col = GetComponent<Collider>();
+	}
+
+	public void Grab(Transform newParent, Vector3 offset)
+	{
+		originalParent = transform.parent;
+		interpolateMode = rbody.interpolation;
+		rbody.isKinematic = true;
+		rbody.interpolation = RigidbodyInterpolation.None;
+		col.enabled = false;
+		transform.SetParent(newParent);
+		transform.localPosition = offset;
+	}
+
+	public void Grab(Transform newParent)
+	{
+		Grab(newParent, Vector3.zero);
+	}
+
+	public void Release(Vector3 velocity)
+	{
+		transform.SetParent(originalParent);
+
+		rbody.interpolation = interpolateMode;
+		rbody.isKinematic = false;
+		col.enabled = true;
+		rbody.velocity = velocity;
+	}
 }
