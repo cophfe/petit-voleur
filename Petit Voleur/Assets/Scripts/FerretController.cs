7cd5d5869c666e52e1e04392f5808bea5b619470
cheese <rahul.jhugroo99@gmail.com>
2021-09-02 02:17:02 +1000

    Cleaned up FerretController script -Commented and fixed some functionality -Fixed fork collider -Added ragdoll button

--- Petit Voleur/Assets/Scripts/FerretController.cs
+++ Petit Voleur/Assets/Scripts/FerretController.cs
@@ -36,7 +36,7 @@ public class FerretController : MonoBehaviour
 	public float airControl = 0.4f;
 	public float lookSpeed = 600f;
 	//Measured in dot product
-	public float maxVerticalAngle = 0.9f;
+	public float maxVelocityFollowAngle = 0.9f;
 	public float gravity = 10;
 	public float maxVelocity = 300.0f;
 
@@ -64,9 +64,10 @@ public class FerretController : MonoBehaviour
 	private FerretPickup ferretPickup;
 	private float friction = 80;
 	private Vector3 forward;
+	private Vector3 projectedInput;
 	private Vector3 targetVelocity;
-	private Vector3 frameAcceleration;
-	private float accelerationScaled;
+	private Vector3 deltaVelocity;
+	private float accelerationThisFrame;
 
     // Start is called before the first frame update
     void Start()
@@ -84,160 +85,42 @@ public class FerretController : MonoBehaviour
 		if (isRagdolled)
 			return;
 
-		//Decrement dash timer
-		if (dashCDTimer > 0)
+		Move();
+
+		DoRotation();
+
+		//Decrement dash timer when on the ground
+		if (grounded && dashCDTimer > 0)
 			dashCDTimer -= Time.deltaTime;
-
-		//GRAVITY
-		velocity -= upDirection * gravity * Time.deltaTime;
-
-		
-		//Reset these
-		targetVelocity = Vector3.zero;
-		frameAcceleration = Vector3.zero;
-		accelerationScaled = acceleration;
-
-		//Calculate floor normal and ground check
-			//Don't consider surfaces that are too steep
-		RaycastHit rayhit;
-		if (Physics.SphereCast(transform.position, characterController.radius * groundCheckRadiusFactor, -floorNormal, out rayhit, groundCheckDistance, groundCheckLayerMask)
-			 && Vector3.Angle(upDirection, rayhit.normal) <= characterController.slopeLimit)
-		{
-			floorNormal = rayhit.normal;
-			floorObject = rayhit.collider;
-			grounded = true;
-		}
-		else
-		{
-			//Reset floor normal
-			StopClimbing();
-			floorNormal = upDirection;
-			floorObject = null;
-			grounded = false;
-		}
-		
-		//Component of velocity that is parallel with the ground
-		Vector3 planarVelocity = Vector3.ProjectOnPlane(velocity, floorNormal);
-		//Project camera's forward vector on virtual plane that is the "floor", use global up vector if wall climbing
-		Vector3 desiredForwardVector;
-		desiredForwardVector = isClimbing? Vector3.up : Camera.main.transform.forward;
-		forward = Vector3.ProjectOnPlane(desiredForwardVector, floorNormal).normalized;
-
-		if (input.sqrMagnitude > 0)
-		{
-			// ~~~~~~~ Generate target velocity ~~~~~~ //
-			//Forward component
-			targetVelocity = forward * input.y;
-			//Cross product the forward and floorNormal to get the right component
-			targetVelocity -= Vector3.Cross(forward, floorNormal) * input.x;
-			targetVelocity *= targetSpeed;
-			
-			//Desired change in velocity
-			frameAcceleration = targetVelocity - planarVelocity;
-			
-			//Give a boost if trying to go away from current velocity
-			if (Vector3.Dot(frameAcceleration, planarVelocity) < 0)
-			{
-				accelerationScaled = friction;
-			}
-		}
-		else
-		{
-			//Add friction
-			if (grounded)
-			{
-				frameAcceleration = -planarVelocity;
-				accelerationScaled = friction;
-			}
-		}
-
-		//Reduce control in the air
-		if (!grounded)
-			accelerationScaled *= airControl;
-		
-		//Amount of acceleration in this frame
-		accelerationScaled *= Time.deltaTime;
-
-		//Limit change in position to the player's acceleration in this frame
-		if (frameAcceleration.sqrMagnitude > accelerationScaled * accelerationScaled)
-		{
-			frameAcceleration = frameAcceleration.normalized * accelerationScaled;
-		}
-
-		//Jump reset check
-		if (isJumping)
-		{
-			//If the player is falling downwards
-			if (Vector3.Dot(velocity, upDirection) <= 0)
-			{
-				CancelJump();
-			}
-		}
-
-		//Add the acceleration calculated this frame to velocity
-		velocity += frameAcceleration;
-		
-		//Limit velocity so nothing breaks
-		if (velocity.sqrMagnitude > maxVelocity * maxVelocity)
-		{
-			velocity = velocity.normalized * maxVelocity;
-		}
-		
-		//lets goooooooooooooooooooooo
-		characterController.Move(velocity * Time.deltaTime);
-
-		//Rotation
-		Vector3 newLookDirection = lookDirection;
-		//Follow the target direction when the player is grounded and trying to move
-		if (grounded && targetVelocity.sqrMagnitude > 0.01f)
-		{
-			newLookDirection = targetVelocity.normalized;
-		}
-		else
-		{
-			//Set direction to direction of velocity unless velocity isn't high enough
-			if (velocity.sqrMagnitude > 0.01f)
-			{
-				//If velocity is less than the maxverticalangle then point in the direction of velocity
-				//Otherwise create a new look direction by adding the current velocity to the current look direction which
-				//	allows for vertical influence on the look direction.
-				//Without this the player will either look straight when jumping or look directly up and get confused
-				Vector3 velDirection = velocity.normalized;
-				if (Mathf.Abs(Vector3.Dot(velDirection, -floorNormal)) < maxVerticalAngle)
-					newLookDirection = velDirection;
-				else
-					newLookDirection = (velDirection + lookDirection).normalized;
-			}
-			else
-			{
-				newLookDirection = Vector3.ProjectOnPlane(lookDirection, floorNormal);
-			}
-		}
-
-		if (Mathf.Abs(Vector3.Dot(newLookDirection, -floorNormal)) < maxVerticalAngle)
-			lookDirection = newLookDirection;
-
-		transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookDirection, floorNormal), lookSpeed * Time.deltaTime);
     }
 
+	// ========================================================|
+	//		--- Collision Response ---
+	//--------------------------------------------------------/
 	void OnControllerColliderHit(ControllerColliderHit hit)
 	{
+		//-----------------------------------------------------------------
 		//Remove component of velocity that is pointing into the collision
-		/*----------------------------------------------------------------
+		/*-----------------------------------------------------------------
 						 \
 						  \
 						   \
 							\--------------->
 		-----------------------------------------------------------------*/
+		
+		//Calculate the component of velocity that is going INTO the wall
 		Vector3 impactVector = -hit.normal * Mathf.Max(-Vector3.Dot(hit.normal, velocity), 0);
 		
+		//Apply forces to other rigidbody if the other object has a rigidbody
 		if (hit.rigidbody)
 		{
+			//If the player is standing on a physics object, then impart all velocity into the object. Remove that same velocity from the player
 			if (hit.collider == floorObject)
 			{
 				hit.rigidbody.AddForceAtPosition(impactVector * defaultImpactMultiplier, hit.point, ForceMode.Impulse);
 				velocity -= impactVector;
 			}
+			//Otherwise add the force over time
 			else
 			{
 				hit.rigidbody.AddForceAtPosition(impactVector * defaultImpactMultiplier, hit.point, ForceMode.Force);
@@ -250,24 +133,193 @@ public class FerretController : MonoBehaviour
 		}
 	}
 
+	// ========================================================|
+	//		--- Main Movement Loop ---
+	//--------------------------------------------------------/
+	void Move()
+	{
+		//GRAVITY
+		//Happens before everything else so that slope calculations can include gravity and resolve it BEFORE rotation happens
+		velocity -= upDirection * gravity * Time.deltaTime;
+
+		//Reset these
+		projectedInput = Vector3.zero;
+		targetVelocity = Vector3.zero;
+		deltaVelocity = Vector3.zero;
+		accelerationThisFrame = acceleration;
+
+		//Calculate floor normal and ground check
+		//Don't consider surfaces that are too steep
+		RaycastHit rayhit;
+		if (Physics.SphereCast(transform.position, characterController.radius * groundCheckRadiusFactor, -floorNormal, out rayhit, groundCheckDistance, groundCheckLayerMask)
+			 && Vector3.Angle(upDirection, rayhit.normal) <= characterController.slopeLimit)
+		{
+			floorNormal = rayhit.normal;
+			floorObject = rayhit.collider;
+			grounded = true;
+		}
+		else
+		{
+			//Reset floor normal
+			StopClimbing();
+			floorObject = null;
+			grounded = false;
+		}
+				
+		//Project camera's forward vector on virtual plane that is the "floor", use global up vector if wall climbing
+		//This is great for slopes, as the player will go up and down the slope, rather than trying to go into it
+		Vector3 desiredForwardVector;
+		desiredForwardVector = isClimbing? Vector3.up : Camera.main.transform.forward;
+		forward = Vector3.ProjectOnPlane(desiredForwardVector, floorNormal).normalized;
+
+		//  Component of velocity that is parallel with the ground  //
+		//Necessary to ensure friction doesn't affect "vertical" component of velocity
+		Vector3 planarVelocity = Vector3.ProjectOnPlane(velocity, floorNormal);
+
+		// ====================================================== //
+		// === //  Calculating Desired Change in Velocity  // === //
+		// ====================================================== //
+			//Desired change in velocity is defined by the difference between how fast we are going and how fast we want to go.
+			//By using this desired change in velocity every frame, but limited by the max acceleration per frame we can ensure
+			//that constant acceleration is maintained, and the velocity never overshoots
+		
+		//Input
+		if (input.sqrMagnitude > 0)
+		{
+			// ~~~~~~~ Generate target velocity ~~~~~~ //
+			projectedInput = forward * input.y;									//Forward component
+			projectedInput -= Vector3.Cross(forward, floorNormal) * input.x;	//Cross product the forward and floorNormal to get the left component
+			targetVelocity = projectedInput * targetSpeed;						//Multiply by target speed to set the magnitude of targetVelocity
+			
+			//Desired change in velocity along the plane
+			deltaVelocity = targetVelocity - planarVelocity;
+			
+			// ~~~~~~~~~~~~~~~ Backkick ~~~~~~~~~~~~~~ //
+			//Give a boost if trying to go away from current velocity. Feels better.
+			if (Vector3.Dot(deltaVelocity, planarVelocity) < 0)
+			{
+				accelerationThisFrame = friction;
+			}
+		}
+		//No input
+		else
+		{
+			// ~~~~~~~~~~~~~ Add Friction ~~~~~~~~~~~~ //
+			//Set the desired change in velocity to the inverse of the current planar velocity
+			if (grounded)
+			{
+				deltaVelocity = -planarVelocity;
+				accelerationThisFrame = friction;
+			}
+		}
+	
+		//Reduce control in the air
+		if (!grounded)
+			accelerationThisFrame *= airControl;
+		
+		//Finally limit acceleration value to be for this frame
+		accelerationThisFrame *= Time.deltaTime;
+
+		//Limit change in position to accelerationThisFrame
+		if (deltaVelocity.sqrMagnitude > accelerationThisFrame * accelerationThisFrame)
+		{
+			deltaVelocity = deltaVelocity.normalized * accelerationThisFrame;
+		}
+
+		//Reset jump if the player has reached the peak of the jump (player is now falling)
+		if (isJumping && Vector3.Dot(velocity, upDirection) <= 0)
+			CancelJump();
+
+		//Add the acceleration calculated this frame to velocity
+		velocity += deltaVelocity;
+		
+		//Hard limit to totalVelocity
+		if (velocity.sqrMagnitude > maxVelocity * maxVelocity)
+		{
+			velocity = velocity.normalized * maxVelocity;
+		}
+		
+		//lets goooooooooooooooooooooo
+		characterController.Move(velocity * Time.deltaTime);
+	}
+
+	// ========================================================|
+	//		--- Character Rotation ---
+	//--------------------------------------------------------/
+	void DoRotation()
+	{
+		//Propose a new lookDirection
+		Vector3 newLookDirection = lookDirection;
+
+		//Follow the target direction when the player is grounded and trying to move
+		if (grounded)
+		{
+			if (projectedInput.sqrMagnitude > 0.01f)
+				newLookDirection = projectedInput;
+			//Set the new lookDirection to be the same as the old one but projected on the floor
+			//This is great for situations where the player changes surfaces
+			else
+				newLookDirection = Vector3.ProjectOnPlane(lookDirection, floorNormal);
+			
+		}
+		//Set direction to direction of velocity unless velocity isn't high enough
+		else if (velocity.sqrMagnitude > 0.01f)
+		{
+			//If velocity is less than the maxVelocityFollowAngle then point in the direction of velocity
+			//Otherwise create a new look direction by adding the current velocity to the current look direction which
+			//	allows for vertical influence on the look direction.
+			//Without this the player will either look straight when jumping or look directly up and get confused
+			Vector3 velDirection = velocity.normalized;
+			float velAngle = Mathf.Abs(Vector3.Angle(velDirection, floorNormal) - 90);
+			if (velAngle < maxVelocityFollowAngle)
+				newLookDirection = velDirection;
+			else
+				newLookDirection = (velDirection + lookDirection).normalized;
+		}
+
+		float lookDirAngle = Mathf.Abs(Vector3.Angle(newLookDirection, floorNormal) - 90);
+
+		if (lookDirAngle < maxVelocityFollowAngle)
+			lookDirection = newLookDirection;
+		else
+			lookDirection = forward;
+
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookDirection, floorNormal), lookSpeed * Time.deltaTime);
+	}
+
+	// ========================================================|
+	//		--- Start Dash ---
+	//--------------------------------------------------------/
+	//Starts a dash if the dash cooldown is done
 	void Dash()
 	{
 		if (dashCDTimer <= 0)
 		{
-			Vector3 dashDirection = forward;
+			//Default to lookDirection
+			Vector3 dashDirection = lookDirection;
+			//Use projected input if possible
 			if (input.sqrMagnitude > 0)
-				dashDirection = targetVelocity.normalized;
+				dashDirection = projectedInput;
 
 			velocity = dashDirection * dashVelocity;
 			dashCDTimer = dashCooldown;
 		}
 	}
 
+	// ========================================================|
+	//		--- Reset Dash ---
+	//--------------------------------------------------------/
+	//Simply clears the dash cooldown
 	void ResetDash()
 	{
 		dashCDTimer = 0;
 	}
 
+	// ========================================================|
+	//		--- Starts a jump ---
+	//--------------------------------------------------------/
+	//Change gravity value based on the jump arc
+	//Removes the vertical component of velocity and adds an impulse based on jump arc
 	void StartJump()
 	{
 		gravity = jumpArc.GetGravity();
@@ -277,52 +329,78 @@ public class FerretController : MonoBehaviour
 		isJumping = true;
 	}
 
+	// ========================================================|
+	//		--- Cancels a jump ---
+	//--------------------------------------------------------/
+	//Resets gravity to normal and cancels the jump
 	void CancelJump()
 	{
 		gravity = fallingArc.GetGravity();
 		isJumping = false;
 	}
 
+	// ========================================================|
+	//		--- Try to start climbing ---
+	//--------------------------------------------------------/
+	//Will cast spheres to check surrounding walls and try to stick to one
 	bool TryClimb()
 	{
+		//No need to climb if youo're already climbing
 		if (!isClimbing)
 		{
 			RaycastHit rayhit;
-
+			//Initial rayDirection for the first pass
 			Vector3 rayDirection = Vector3.forward;
+			//A quaternion that is rotated on the y axis by an amount to evenly spread out rayDirections by the number of wallCheckAngles
+			Quaternion evenRotation = Quaternion.Euler(0, 360.0f / (float)wallCheckAngles, 0);
 
 			for (int i = 0; i < wallCheckAngles; ++i)
 			{
-				Debug.DrawRay(transform.position, rayDirection * 3, Color.red, 2);
+				//Casts a sphere in rayDirection
 				if (Physics.SphereCast(transform.position, characterController.radius * wallCheckFactor, rayDirection, out rayhit, wallCheckDistance, climbableLayers))
 				{
 					StartClimbing(rayhit.normal);
 					return true;
 				}
-				rayDirection = Quaternion.Euler(0, 360.0f / (float)wallCheckAngles, 0) * rayDirection;
+
+				//Rotate the ray
+				rayDirection = evenRotation * rayDirection;
 			}
 		}
 
 		return false;
 	}
 
+	// ========================================================|
+	//		--- Start Climbing ---
+	//--------------------------------------------------------/
+	//Begins a climb where newUp is the newUpdirection or the normal of the plane
+	//Also changes friction value
 	void StartClimbing(Vector3 newUp)
 	{
 		isClimbing = true;
 		upDirection = newUp;
 		floorNormal = newUp;
 		friction = climbFriction;
-		CancelJump();
 	}
-
+	// ========================================================|
+	//		--- Resets Climbing ---
+	//--------------------------------------------------------/
+	//Resets the upDirection and floorNormal, and resets friction
 	void StopClimbing()
 	{
 		isClimbing = false;
 		upDirection = Vector3.up;
-		floorNormal = Vector3.up;
+		floorNormal = upDirection;
 		friction = floorFriction;
 	}
 
+	// ========================================================|
+	//		--- RAGDOLL MOMENT ---
+	//--------------------------------------------------------/
+	//Disables character controller and enables physics when true
+	//Enables character controller and disables physics when false
+	//Preserves velocity between states
 	public void SetRagdollState(bool state)
 	{
 		isRagdolled = state;
@@ -331,7 +409,9 @@ public class FerretController : MonoBehaviour
 		
 		if (!isRagdolled)
 		{
+			rigidbody.interpolation = RigidbodyInterpolation.None;
 			velocity = rigidbody.velocity;
+			//This is important to prevent the player from having incorrect gravity
 			StopClimbing();
 		}
 
@@ -340,8 +420,13 @@ public class FerretController : MonoBehaviour
 		if (isRagdolled)
 		{
 			rigidbody.velocity = velocity;
+			rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
 		}
 	}
+
+	// ====================================================== //
+	// =================== Input Checking =================== //
+	// ====================================================== //
 
 	public void OnMoveAxis(InputValue value)
 	{
@@ -367,7 +452,14 @@ public class FerretController : MonoBehaviour
 		Dash();
 	}
 
-	//---------STRUCTS-------------------//
+	public void OnRagdoll()
+	{
+		SetRagdollState(!isRagdolled);
+	}
+
+	// ##################################################################### //
+	// ############################## STRUCTS ############################## //
+	// ##################################################################### //
 
 	[System.Serializable]
 	public struct JumpArc
